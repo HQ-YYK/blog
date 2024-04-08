8250d67345be8573e8b868601a63e4c3f4bf2d52
lyxlwz <1850115720@qq.com>
2024-04-08 11:18:53 +0800

    loading

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 
 代码参考 [threejs官网例子games_fps](https://threejs.org/examples/#games_fps)
 [模型提供](https://www.aplaybox.com/details/model/hYYrP5YeFcUq)
+[loading 学习提供](https://juejin.cn/post/7271070291689750582?searchId=202404080951001F6A7573B43FA9511E80)
--- src/pages/genshin/Preloader.tsx
+++ src/pages/genshin/Preloader.tsx
@@ -1,21 +1,104 @@
 "use client"
-import { useRef } from 'react'
+import { SetStateAction, useRef, useState } from 'react'
 import "./Preloader.css";
 
 const Preloader = ({ progress }: { progress: number }) => {
   const progressRef = useRef<HTMLDivElement>(null);
+  // 用于判断是否正在拖动
+  const [isDragging, setIsDragging] = useState(false);
+  // 鼠标Y坐标
+  const [initialY, setInitialY] = useState(0);
+  // 旋转角度（Y轴）
+  const [initialRotationY, setInitialRotationY] = useState(0);
+
+  const handleMouseDown = (event: { clientY: SetStateAction<number>; }) => {
+    setInitialY(event.clientY);
+    const elem = progressRef.current;
+    if (elem) {
+      const computedStyle = window.getComputedStyle(elem);
+      const transform = computedStyle.getPropertyValue('transform').replace(/[^0-9-.,]/g, '').split(',');
+      setInitialRotationY(parseInt(transform[4]));
+      setIsDragging(true);
+    }
+  };
+
+  const handleMouseMove = (event: { clientY: any; }) => {
+    const elem = progressRef.current;
+    if (isDragging && elem) {
+      const currentY = event.clientY;
+      const dy = currentY - initialY;
+      const newRotationY = initialRotationY + dy * -1;
+      elem.style.transform = `rotateX(${newRotationY}deg)`;
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
 
   return (
-    <div className="progress-container" style={{ opacity: progress != 100.00 ? "1" : "0" }}>
-      <div className="progress-content" >
-        <img className="Genshin" src={"img/genshin/Genshin.png"} alt="图片" style={{ opacity: progress != 100.00 ? "1" : "0" }} />
-        <div className="LoadingBar" style={{ opacity: progress != 100.00 ? "1" : "0" }}>
-          <div className="progress-bar">
-            <div ref={progressRef} className="progress" style={{ width: progress + "%" }}></div>
+    <div className="box">
+      <div
+        className="main1 flex"
+        ref={progressRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
+        <div className="main2">
+          <div className="firstCircle positon center"></div>
+          {/* 最外层的圆环 */}
+          <div className="secondCircle positon center"></div>
+          {/* 第二层两段圆环 */}
+          <div className="fourthCircle flex positon center">
+            {/* 一堆圆环组成的圆环 */}
+            <div className="annulusContent">
+              <div className="annulus annulus1"></div>
+              {/* 每一个小圆环 */}
+              <div className="annulus annulus2"></div>
+              <div className="annulus annulus3"></div>
+              <div className="annulus annulus4"></div>
+              <div className="annulus annulus5"></div>
+              <div className="annulus annulus6"></div>
+              <div className="annulus annulus7"></div>
+              <div className="annulus annulus8"></div>
+              <div className="annulus annulus9"></div>
+              <div className="annulus annulus10"></div>
+              <div className="annulus annulus11"></div>
+              <div className="annulus annulus12"></div>
+              <div className="annulus annulus13"></div>
+              <div className="annulus annulus14"></div>
+              <div className="annulus annulus15"></div>
+              <div className="annulus annulus16"></div>
+              <div className="annulus annulus17"></div>
+              <div className="annulus annulus18"></div>
+              <div className="annulus annulus19"></div>
+              <div className="annulus annulus20"></div>
+              <div className="annulus annulus21"></div>
+              <div className="annulus annulus22"></div>
+              <div className="annulus annulus23"></div>
+              <div className="annulus annulus24"></div>
+              <div className="annulus annulus25"></div>
+              <div className="annulus annulus26"></div>
+              <div className="annulus annulus27"></div>
+              <div className="annulus annulus28"></div>
+              <div className="annulus annulus29"></div>
+              <div className="annulus annulus30"></div>
+              <div className="annulus annulus31"></div>
+              <div className="annulus annulus32"></div>
+              <div className="annulus annulus33"></div>
+              <div className="annulus annulus34"></div>
+              <div className="annulus annulus35"></div>
+              <div className="annulus annulus36"></div>
+            </div>
           </div>
-          <div className="ball" style={{ left: "-2vmin" }}></div>
-          <div className="ball" style={{ left: "40.8vmin", top: "-2.4vmin" }}></div>
-          <div style={{ fontSize: "1.4vmin", position: "relative", left: "42.8vmin", top: "-4.0vmin" }}>{progress + "%"}</div>
+          <div className="fifthCircle positon center"></div>
+          <div className="sixthCircle positon center"></div>
+          <div className="seventhCircle positon center"></div>
+        </div>
+
+        <div className="main3">
+          <div className='progress'>{progress}%</div>
         </div>
       </div>
     </div>
